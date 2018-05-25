068cfca09451cc0fc2ca2aab3d60f63a2fbce145
kakadila <32809434+kakadila@users.noreply.github.com>
2018-05-25 23:01:31 +0800

    Update evserver.cpp

--- gohangServer/evserver.cpp
+++ gohangServer/evserver.cpp
@@ -1,12 +1,23 @@
-/*
-  This exmple program provides a trivial server program that listens for TCP
-  connections on port 9995.  When they arrive, it writes a short message to
-  each client connection, and closes each connection once it is flushed.
+<program>  Copyright (C) 2017/12/12  kakadila
+    This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
+    This is free software, and you are welcome to redistribute it
+    under certain conditions; type `show c' for details.
 
-  Where possible, it exits cleanly in response to a SIGINT (ctrl-c).
-*/
+The hypothetical commands `show w' and `show c' should show the appropriate
+parts of the General Public License.  Of course, your program's commands
+might be different; for a GUI interface, you would use an "about box".
 
+  You should also get your employer (if you work as a programmer) or school,
+if any, to sign a "copyright disclaimer" for the program, if necessary.
+For more information on this, and how to apply and follow the GNU GPL, see
+<http://www.gnu.org/licenses/>.
 
+  The GNU General Public License does not permit incorporating your program
+into proprietary programs.  If your program is a subroutine library, you
+may consider it more useful to permit linking proprietary applications with
+the library.  If this is what you want to do, use the GNU Lesser General
+Public License instead of this License.  But first, please read
+<http://www.gnu.org/philosophy/why-not-lgpl.html>.
 #include <string.h>
 #include <errno.h>
 #include <stdio.h>
